4ddc843b5cc3c54c64aa18eb793d475d4b38ce01
mdabarik <mdabarik19@gmail.com>
2026-01-14 01:08:43 +0600

    project setup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace inventory_management_system_dotnet_mvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
