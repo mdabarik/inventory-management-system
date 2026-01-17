8c24a3c1459a466f785b2e012a0f9ca3002c611f
mdabarik <mdabarik19@gmail.com>
2026-01-18 01:45:58 +0600

    product route added

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using InventoryManagementSystem.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace InventoryManagementSystem.Controllers
+{
+    [Route("[controller]")]
+    public class ProductController : Controller
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet] 
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            return View(ProductRepository.GetAll());
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
