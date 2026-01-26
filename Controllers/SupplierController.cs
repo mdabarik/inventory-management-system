813916aabd39bb3497bf6b2a16686a9479202683
mdabarik <mdabarik19@gmail.com>
2026-01-27 00:15:00 +0600

    active navigation added

--- Controllers/SupplierController.cs
+++ Controllers/SupplierController.cs
@@ -2,11 +2,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace InventoryManagementSystem.Controllers
 {
-    public class SupplierController
+    public class SupplierController: Controller
     {
-        
-    }
+        private readonly ILogger<SupplierController> _logger;
+        public SupplierController(ILogger<SupplierController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+     }
 }
--- obj/Debug/net9.0/InventoryManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net9.0/InventoryManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65cf34470431b2d5d253fa80ad267b4e60d68f1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4407af182ac649d9f12a966bc44cfc86610ce449")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
