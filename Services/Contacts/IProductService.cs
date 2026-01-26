0af10e87be6f3c59446593cf1d88cde6d4bdd02f
mdabarik <mdabarik19@gmail.com>
2026-01-27 00:32:29 +0600

    product service added

--- Services/Contacts/IProductService.cs
+++ Services/Contacts/IProductService.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using InventoryManagementSystem.Models;
 
 namespace InventoryManagementSystem.Services.Contacts
 {
-    public interface IProductService
-    {
-        
+    public interface IProductService 
+    { 
+        Task<IEnumerable<Product>> GetAllProductAsync(); 
+        Task<Product?> GetProductByIdAsync(int id); 
+        Task<Product> AddProductAsync(Product product); 
+        Task<bool> UpdateProductAsync(Product product); 
+        Task<bool> DeleteProductByIdAsync(int id); 
     }
 }
--- Services/Implementations/ProductService.cs
+++ Services/Implementations/ProductService.cs
@@ -1,12 +1,56 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using InventoryManagementSystem.Models;
+using InventoryManagementSystem.Services.Contacts;
+using InventoryManagementSystem.Data;
 
 namespace InventoryManagementSystem.Services.Implementations
 {
-    public class ProductService
+    public class ProductService : IProductService
     {
-        
+        private readonly IMSDBContext _context;
+
+        public ProductService(IMSDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Product>> GetAllProductAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<Product?> GetProductByIdAsync(int id)
+        {
+            return await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<Product> AddProductAsync(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<bool> UpdateProductAsync(Product product)
+        {
+            var exists = await _context.Products.AnyAsync(p => p.Id == product.Id);
+            if (!exists) return false;
+
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> DeleteProductByIdAsync(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null) return false;
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            return true;
+        }
     }
 }
