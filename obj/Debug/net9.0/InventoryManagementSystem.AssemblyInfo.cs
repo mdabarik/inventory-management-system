471068f4d0f81cfc04dab6e60e614b39774c9f1f
mdabarik <mdabarik19@gmail.com>
2026-01-28 16:00:23 +0600

    mini update

--- obj/Debug/net9.0/InventoryManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net9.0/InventoryManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4150b49d1d38c63f761dcbbddd3dbeb0e8dfe81c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14bdd0730a3796b10129f3c7da2e3fbd5e66b222")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
